325c938e1d0fd47acbf0ac24fe5e0aa07661b9be
StephSteve <stephanie.oncallbusiness@gmail.com>
2020-09-10 07:13:12 -0400

    update

--- src/main/java/com/peerlender/profile/ProfileApplication.java
+++ src/main/java/com/peerlender/profile/ProfileApplication.java
@@ -1,11 +1,16 @@
 package com.peerlender.profile;
 
+import com.peerlender.profile.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfileApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProfileApplication.class, args);
@@ -14,6 +19,7 @@ public class ProfileApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		userRepository.save()
 
 	}
 }
